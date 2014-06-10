43015fffda390521e12cfbbaa2d22141692e2dc4
rtm9zc <rtm9zc@virginia.edu>
2014-06-10 15:53:12 -0400

    fixed build for macs

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ echo Structuring webpage...
 cp book/index.html index.html
 rm -rf gitbook
 rm -rf md
-cp -ar book/gitbook gitbook
-cp -ar book/md md
+cp -r book/gitbook gitbook
+cp -r book/md md
 cp book/index.html index.html
 cp book/manifest.appcache manifest.appcache
 cp book/search_index.json search_index.json
